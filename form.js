8d83bceaaf7038157e9949384c448e738f1ea10c
CHR-onicles <tpandivine48@gmail.com>
2021-12-31 17:55:06 +0000

    fixed form nav btns not working... and reformatted css and js

--- form.js
+++ form.js
@@ -1,65 +1,73 @@
- let currentTab=0;
- ShowForm(currentTab);
-function ShowForm(n){
-    var form=document.getElementsByClassName('form');
-    form[n].style.display='flex';
+let currentTab = 0;
 
-    if (n==0){
-        document.getElementById('previous').style.display='none';
-    }else{
-        document.getElementById('previous').style.display='flex';
+ShowForm(currentTab);
+
+function ShowForm(n) {
+    var form = document.getElementsByClassName('form');
+    form[n].style.display = 'flex';
+
+    if (n == 0) {
+        form[n].querySelector('.previous-btn').style.display = 'none';
+    } else {
+        form[n].querySelector('.previous-btn').style.display = 'flex';
     }
 
-    if(n==(form.length-1)){
-        document.getElementById('next').innerHTML='Submit';
-    }else{
-        document.getElementById('next').innerHTML='Next';
+    if (n == (form.length - 1)) {
+        form[n].querySelector('.next-btn').innerHTML = 'Submit';
+    } else {
+        form[n].querySelector('.next-btn').innerHTML = 'Next';
     }
 
-    regulateStepIndicator(n)
-
-    
+    regulateStepIndicator(n);
 }
 
-var move=document.getElementsByClassName('nav-btns') ;
+prevBtns = [...document.getElementsByClassName('previous-btn')];
+prevBtns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        nextTab(-1);
+    });
+});
 
- for(i=0;i<move.length;i++){
- move[i].addEventListener('click',function nextTab(n){
-    
-    var form =document.getElementsByClassName('form');
-    if(n==0 && !validateForm()) return false;
-    form[currentTab].style.display='none';
-    currentTab= currentTab+1;
+nextBtns = [...document.getElementsByClassName('next-btn')];
+nextBtns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        nextTab(1);
+    });
+});
 
-    if(currentTab>=form.length){
+function nextTab(n) {
+    var form = document.getElementsByClassName('form');
+    if (n == 0 && !validateForm()) return false;
+    form[currentTab].style.display = 'none';
+    currentTab = currentTab + n;
+
+    if (currentTab >= form.length) {
         document.getElementById('donationform').submit();
         return false;
     }
     ShowForm(currentTab);
-});
- }
+}
 
-function validateForm(){
-    var form,input,i=true;
-    var form=document.getElementsByClassName('form');
-    var input= document.getElementsByTagName('input');
-    for(i=0,i<input.length;i++;) {
-    if(input[i].value=""){
-        input[i].className +='invalid';
-        valid=false;
+function validateForm() {
+    var form, input, i = true;
+    var form = document.getElementsByClassName('form');
+    var input = document.getElementsByTagName('input');
+    for (i = 0, i < input.length; i++;) {
+        if (input[i].value = "") {
+            input[i].classList.add('invalid');
+            valid = false;
+        }
     }
-    }
-    if(valid){
-        document.getElementsByClassName('check')[currentTab].className+='completed';
+    if (valid) {
+        document.getElementsByClassName('check')[currentTab].classList.add('completed');
     }
     return valid;
 }
 
-function regulateStepIndicator(n){
-var i, check=document.getElementsByClassName('check');
-for(i=0,i<check.length;i++;){
-    check[i].className=check[i].className.replace("completed",'');
-}
-check[n].className += " completed";
-
+function regulateStepIndicator(n) {
+    var i, check = document.getElementsByClassName('check');
+    for (i = 0, i < check.length; i++;) {
+        check[i].classList.remove('completed');
+    }
+    check[n].classList.add('completed');
 }
